70f98df992409df5e15945efed168a756d2d1cfc
Blue01H <juandiegourdanetafuenmayor@gmail.com>
2020-12-10 16:35:21 -0400

    ready for deployment

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app=express();
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/public")));
+}
+
+console.log(__dirname);
+console.log(path.join(__dirname, "client/public"));
+
 app.use("/auth",require("./routes/jwtAuth"));
 app.use("/dashboard", require("./routes/dashboard"));
 
-app.listen(5000, () => {
-    console.log("server is running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
 })
 
 
